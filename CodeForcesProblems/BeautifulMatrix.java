a6928f9659be716c2219b942d3b6ce8e9170d45d
Shariq Shaikh <62669036+shariq-hub@users.noreply.github.com>
2021-10-04 22:53:25 +0500

    Update BeautifulMatrix.java
    Problem link: http://codeforces.com/problemset/problem/263/A

--- CodeForcesProblems/BeautifulMatrix.java
+++ CodeForcesProblems/BeautifulMatrix.java
@@ -4,8 +4,8 @@ public class Problem269a{
     public static void main(String[] args) {
  
       Scanner scanner=new Scanner(System.in);
-     int a=3;
-     int b=3;
+     int a=3; // the index location for row
+     int b=3;  // the index location for column
      int input=0;
      int count1=0;
      int count2=0;
@@ -14,9 +14,9 @@ public class Problem269a{
          for(int j=1; j<6; j++){
          input=scanner.nextInt();
          if(input==1){
-             count1=Math.abs(a-i);
-             count2=Math.abs(j-b);
-             count3=count1+count2;
+             count1=Math.abs(a-i); // we subtract the value of row where we find one in the row to get the row location
+             count2=Math.abs(j-b); // we subtract the value of column where we find one in the column to get the column location
+             count3=count1+count2; // then we add both of them to get the exact location.
              break;
          }
      }
