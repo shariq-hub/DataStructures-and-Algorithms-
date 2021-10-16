9dd48ca0eecb1accb00b57ab82d58d4676348260
Shariq Shaikh <62669036+shariq-hub@users.noreply.github.com>
2021-10-17 00:57:00 +0500

    Update Beautiful year.java

--- CodeForcesProblems/Beautiful year.java
+++ CodeForcesProblems/Beautiful year.java
@@ -6,11 +6,12 @@ public class Problem166{
         year=scanner.nextInt();
         while(true){
             year+=1;
-        int a=year/1000;
-        int b=year/100%10;
-        int c=year/10%10;
-        int d=year%10;
-        if(a!=b&&a!=c&&a!=d&&b!=c&&b!=d&&c!=d){
+        int a=year/1000; // we will get first digit
+        int b=year/100%10; // we will get secind digit
+        int c=year/10%10; //we will get third digit
+        int d=year%10; // we will get the last digit
+        if(a!=b&&a!=c&&a!=d&&b!=c&&b!=d&&c!=d) // here we are comparing each digit
+        { 
             System.out.println(year);
             break;
         }
