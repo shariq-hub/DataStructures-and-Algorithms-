0ad7c309dd966719214c30f32d239b17ee95cf6b
Shariq Shaikh <shariqshaikh226@gmail.com>
2022-02-20 13:46:40 +0500

    Create CyclicallyRotateAnArraybyOne.java

---
+++ BasicProblems/CyclicallyRotateAnArraybyOne.java
@@ -0,0 +1,19 @@
+
+//Problem link:https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1#
+// Solution in java
+public class queueimplementation{
+    public static void main(String[] args) {
+        int arr[]=new int[]{1,2,3,4,5}; //output is 5,1,2,3,4
+        int length=arr.length;
+        int temp=arr[length-1];
+         for(int i=length-1; i>0; i--) 
+        {
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
+        
+        for(int c:arr){
+        System.out.println(c);
+        }
+    }
+}
